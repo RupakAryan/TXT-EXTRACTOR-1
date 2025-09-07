140931dffe51bbf047019c69396e83ed5d84dc42
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-09-08 00:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello from spidy'
-
+@app.route("/")
+def home():
+    return "Render pe Flask app chal raha hai! ✅"
 
 if __name__ == "__main__":
-    app.run()
-  
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
